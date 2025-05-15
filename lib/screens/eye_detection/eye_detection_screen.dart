8321ab5b9529da0a15e132224a533d63c5d7a8d5
AliceWonerfulWorld <gomaazarasi1789@gmail.com>
2025-05-16 02:32:48 +0900

    [add] Test2

--- lib/screens/eye_detection/eye_detection_screen.dart
+++ lib/screens/eye_detection/eye_detection_screen.dart
@@ -1,4 +1,4 @@
-//自動デプロイのテスト
+//自動デプロイのTest
 import 'package:flutter/material.dart';
 import 'package:camera/camera.dart';
 import 'package:google_mlkit_face_detection/google_mlkit_face_detection.dart';
