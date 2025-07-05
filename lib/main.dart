6bac9e0ff16e6ad1259d319df44f8c68c6642e44
AliceWonerfulWorld <gomaazarasi1789@gmail.com>
2025-07-05 14:07:56 +0900

    [modify] 音量調節

--- lib/main.dart
+++ lib/main.dart
@@ -45,7 +45,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: '寝過ごしゲーム',
+      title: '寝過ごしパニック STAGE3',
       theme: ThemeData(
         // This is the theme of your application.
         //
